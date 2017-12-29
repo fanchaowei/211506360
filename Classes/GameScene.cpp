0cf9bffc54c34bc8589808e91e9daf1f5c1ae604
blodyT <609252861@qq.com>
2017-12-29 21:07:53 +0800

    ...

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -1,4 +1,6 @@
 #include "GameScene.h"
+#include "FishingJoyData.h"
+#include "PersonalAudioEngine.h"
 
 GameScene::GameScene()
 {
@@ -8,19 +10,39 @@ bool GameScene::init()
 {
 	do
 	{
-		if (!CCScene::init())
-		{
-			break;
-		}
-		backgroundLayer = BackgroundLayer::create();
-		CC_BREAK_IF(!backgroundLayer);
-		this->addChild(backgroundLayer);
-		fishLayer = FishLayer::create();
-		CC_BREAK_IF(!fishLayer);
-		this->addChild(fishLayer);
-		menuLayer = MenuLayer::create();
-		CC_BREAK_IF(!menuLayer);
-		CC_SAFE_RETAIN(menuLayer);
+		CC_BREAK_IF(!CCScene::init());
+
+		_backgroundLayer = BackgroundLayer::create();
+		CC_BREAK_IF(!_backgroundLayer);
+		this->addChild(_backgroundLayer);
+
+		_fishLayer = FishLayer::create();
+		CC_BREAK_IF(!_fishLayer);
+		this->addChild(_fishLayer);
+
+		_cannonLayer = CannonLayer::create();
+		CC_BREAK_IF(!_cannonLayer);
+		this->addChild(_cannonLayer);
+
+		_panelLayer = PanelLayer::create();
+		CC_BREAK_IF(!_panelLayer);
+		this->addChild(_panelLayer);
+
+		_menuLayer = MenuLayer::create(); 
+		CC_BREAK_IF(!_menuLayer);
+		CC_SAFE_RETAIN(_menuLayer); 
+
+		_touchLayer = TouchLayer::create();
+		CC_BREAK_IF(!_touchLayer);
+		this->addChild(_touchLayer);
+
+		int gold = FishingJoyData::sharedFishingJoyData()->getGold();
+
+		_panelLayer->getGoldCounter()->setNumber(gold);
+
+		this->scheduleUpdate();
+		
+		PersonalAudioEngine::sharedEngine()->playBackgroundMusic("music_1.mp3", true);
 		return true;
 	} while (0);
 	return false;
@@ -28,5 +50,102 @@ bool GameScene::init()
 
 GameScene::~GameScene()
 {
-	CC_SAFE_RELEASE_NULL(menuLayer);
+	CC_SAFE_RELEASE(_menuLayer);
+}
+
+void GameScene::cannonAimAt(CCPoint target)
+{
+	_cannonLayer->aimAt(target);
+}
+
+void GameScene::cannonShootTo(CCPoint target)
+{
+	int type = _cannonLayer->getWeapon()->getCannon()->getType();
+	int cost = (type+1) * 1;
+	int currentGold = FishingJoyData::sharedFishingJoyData()->getGold();
+	if(currentGold >= cost && _cannonLayer->shootTo(target)){
+		PersonalAudioEngine::sharedEngine()->playEffect("bgm_fire.aif");
+		this->alterGold(-cost);
+	}
+}
+
+bool GameScene::checkOutCollisionBetweenFishesAndBullet(Bullet* bullet)
+{
+	CCPoint bulletPos = bullet->getCollosionPoint();
+	CCArray* fishArray = _fishLayer->getFishArray();
+	CCObject* obj = NULL;
+	CCARRAY_FOREACH(fishArray, obj)
+	{
+		Fish* fish =(Fish*)obj;
+		if(fish->isRunning() && fish->getCollisionArea().containsPoint(bulletPos))
+		{
+			bullet->end();
+			return true;
+		}
+	}
+	return false;
+}
+
+void GameScene::checkOutCollision()
+{
+	CCArray* bullets = _cannonLayer->getWeapon()->getBullets();
+	CCObject* obj = NULL;
+	CCARRAY_FOREACH(bullets, obj)
+	{
+		Bullet* bullet = (Bullet*)obj;
+		if(bullet->isVisible())
+		{
+			if(checkOutCollisionBetweenFishesAndBullet(bullet))
+			{
+				checkOutCollisionBetweenFishesAndFishingNet(bullet);
+			}
+		}
+	}	
+}
+
+void GameScene::update(float delta)
+{
+	checkOutCollision();
+}
+
+void GameScene::fishWillBeCaught(Fish* fish)
+{
+	float weaponPercents[k_Cannon_Count] = { 0.3f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 1.1f };
+	float fishPercents[	k_Fish_Type_Count] = { 1.0f, 0.9f, 0.8f, 0.7f, 0.6f, 0.5f, 0.4f };
+	int cannonType = _cannonLayer->getWeapon()->getCannonType();
+	int fishType = fish->getType();
+	if(CCRANDOM_0_1() < 1.1)
+	{
+		fish->beCaught();
+		PersonalAudioEngine::sharedEngine()->playEffect("bgm_net.mp3");
+		int reward = (fishType+1)*10;
+		this->alterGold(reward);
+	}
+}
+
+void GameScene::checkOutCollisionBetweenFishesAndFishingNet(Bullet* bullet)
+{
+	Weapon* weapon = _cannonLayer->getWeapon();
+	CCRect rect = weapon->getCollisionArea(bullet);
+	CCArray* fishArray = _fishLayer->getFishArray();
+	CCObject* obj = NULL;
+	CCARRAY_FOREACH(fishArray, obj)
+	{
+		Fish* fish = (Fish*)obj;
+		if(fish->isRunning() && rect.intersectsRect(fish->getCollisionArea()))
+		{
+			fishWillBeCaught(fish);
+		}
+	}
+}
+
+void GameScene::alterGold(int delta)
+{
+	FishingJoyData::sharedFishingJoyData()->alterGold(delta);
+	_panelLayer->getGoldCounter()->setNumber(FishingJoyData::sharedFishingJoyData()->getGold());
+}
+
+void GameScene::scheduleTimeUp()
+{
+	this->alterGold(200);
 }
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -3,7 +3,18 @@
 #include "BackgroundLayer.h"
 #include "FishLayer.h"
 #include "MenuLayer.h"
+#include "CannonLayer.h"
+#include "TouchLayer.h"
+#include "Fish.h"
+#include "PanelLayer.h"
+#include "FishingJoyData.h"
+
 USING_NS_CC;
+
+typedef enum{
+	k_Operate_Pause = 0,
+	k_Operate_Resume
+}OperateFlag;
 
 class GameScene :
 	public CCScene
@@ -13,9 +24,22 @@ public:
 	CREATE_FUNC(GameScene)
 	virtual bool init();
 	virtual ~GameScene();
-protected:
-	BackgroundLayer* backgroundLayer;
-	FishLayer* fishLayer;
-	MenuLayer* menuLayer;
-};
+	void cannonAimAt(CCPoint target);
+	void cannonShootTo(CCPoint target);
 
+	void scheduleTimeUp();
+
+protected:
+	BackgroundLayer* _backgroundLayer;
+	FishLayer* _fishLayer;
+	MenuLayer* _menuLayer;
+	CannonLayer* _cannonLayer;
+	TouchLayer* _touchLayer;
+	PanelLayer* _panelLayer;
+	bool checkOutCollisionBetweenFishesAndBullet(Bullet* bullet);
+	void checkOutCollision();
+	virtual void update(float delta);
+	void fishWillBeCaught(Fish* fish);
+	void alterGold(int delta);
+	void checkOutCollisionBetweenFishesAndFishingNet(Bullet* bulet);
+};
